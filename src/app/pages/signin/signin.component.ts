c29a1851ecdc2ce95aca40576211b7cdc8fea0fb
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-04-26 10:37:35 +0530

    Create sign page simple design

---
+++ src/app/pages/signin/signin.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "signin-cmp",
+  moduleId: module.id,
+  templateUrl: "signin.component.html",
+})
+export class SigninComponent implements OnInit {
+  ngOnInit() {}
+}
